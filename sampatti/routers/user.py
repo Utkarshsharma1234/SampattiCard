73cde25805a73e4112cd183a55fc2a873fbda90a
Utkarshsharma1234 <96917045+Utkarshsharma1234@users.noreply.github.com>
2024-01-24 02:23:55 +0530

    backend

--- sampatti/routers/user.py
+++ sampatti/routers/user.py
@@ -28,14 +28,14 @@ def create_domestic_worker(request : schemas.Domestic_Worker, db: Session = Depe
     return userControllers.create_domestic_worker(request, db)
 
 
-@router.post("/domestic_worker/exist/{phoneNumber}")
-def check_existing_worker(phoneNumber, db: Session = Depends(get_db)):
-    return userControllers.check_existing_worker(phoneNumber, db)
+# @router.post("/domestic_worker/exist/{phoneNumber}")
+# def check_existing_worker(phoneNumber, db: Session = Depends(get_db)):
+#     return userControllers.check_existing_worker(phoneNumber, db)
 
 
-@router.post("/login/employer")
-def login_employer(request : schemas.Login_Employer, db : Session = Depends(get_db)):
-    return userControllers.login_employer(request,db)
+# @router.post("/login/employer")
+# def login_employer(request : schemas.Login_Employer, db : Session = Depends(get_db)):
+#     return userControllers.login_employer(request,db)
     
 @router.post("/contract")
 def contract_generation(request : schemas.Contract, db : Session = Depends(get_db)):
